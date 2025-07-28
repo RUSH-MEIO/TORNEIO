e0f2c277e89ddc6f4a2c98bfc6c7a74459cd1888
Paraizo <eduardoparize.05@gmail.com>
2025-07-28 11:12:35 -0300

    att branch

--- torneio.js
+++ torneio.js
@@ -3,13 +3,8 @@ const rl = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-let jogadores = [
-  {
-    jogador1,
-    jogador2,
-    vencedor,
-  },
-];
+let torneios = [];
+
 function exibirMenu() {
   console.log(
     "=========MENU=========\n1-Adicionar Torneio\n2-Listar Torneios\n3-Filtrar Torneio\n4-Deletar Torneios\n5-Registrar Partidas\n6-Listar Partidas de um Torneio\n0-Sair do programa"
@@ -21,7 +16,7 @@ function exibirMenu() {
         adicionarTorneios();
         break;
       case 2:
-        listarTorneios();
+        console.log(torneios);
         break;
       case 3:
         filtrarTorneios();
@@ -30,7 +25,7 @@ function exibirMenu() {
         deletarTorneios();
         break;
       case 5:
-        registrarPartidas();
+        registrapartidas();
         break;
       case 6:
         ListarPartidasDoTorneio();
@@ -43,6 +38,82 @@ function exibirMenu() {
   });
 }
 
+function pergunta(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
+
+async function adicionarTorneios() {
+  console.clear();
+  console.log("==== 🏆 TORNEIO 🏆 ===");
+
+  const INPTorneioNome = await pergunta(
+    "📋 Qual será o nome do torneio que deseja criar?: "
+  );
+  const INPTorneioJogoNome = await pergunta("🎮 Qual será o jogo disputado?: ");
+  const INPTorneioData = await adicionarData();
+  const INPTorneioPlayers = await pergunta(
+    "👤 Quais serão os participantes? (Separe por virgula ex player1,player2,player3): "
+  );
+  adicionarTorneiosArray(
+    INPTorneioNome,
+    INPTorneioJogoNome,
+    INPTorneioData,
+    INPTorneioPlayers
+  );
+}
+
+async function adicionarData() {
+  let dia, mes, ano;
+  let dataValida = false;
+  let timestamp;
+
+  while (!dataValida) {
+    dia = await pergunta("📅 Insira o DIA do torneio (DD): ");
+    mes = await pergunta("📅 Insira o MES do torneio (MM): ");
+    ano = await pergunta("📅 Insira o ANO do torneio (AAAA): ");
+
+    const numDia = parseInt(dia, 10);
+    const numMes = parseInt(mes, 10);
+    const numAno = parseInt(ano, 10);
+    const dataObjeto = new Date(numAno, numMes - 1, numDia);
+    if (
+      !isNaN(dataObjeto.getTime()) &&
+      dataObjeto.getDate() === numDia &&
+      dataObjeto.getMonth() === numMes - 1 &&
+      dataObjeto.getFullYear() === numAno
+    ) {
+      timestamp = dataObjeto.getTime();
+      dataValida = true;
+      //console.log(`Data válida! Timestamp gerado: ${timestamp}`);
+    } else {
+      console.log("⚠️ Data inválida. Por favor, insira uma data válida.");
+    }
+  }
+  return timestamp;
+}
+
+function adicionarTorneiosArray(nome, jogo, timestampID, playersString) {
+  const DataFormatada = new Date(timestampID).toLocaleString("pt-BR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const IDTORNEIO = Date.now();
+  const playersarray = playersString.split(",").map((player) => player.trim());
+
+  torneios.push({
+    id: IDTORNEIO,
+    nome: nome,
+    jogo: jogo,
+    data: DataFormatada,
+    participantes: playersarray,
+  });
+  exibirMenu();
+}
+
+exibirMenu();
+
 function registrarPartidas() {
   torneios.forEach((ID, nome) => {
     console.log(`${ID}- ${nome}`);
@@ -51,8 +122,7 @@ function registrarPartidas() {
     "Insira qual torneio deseja registrar uma partida (ID):\n",
     (idTorneio) => {
       if (torneios.ID.includes(idTorneio)) {
-        const torneioSelecionado = //torneio que tem o ID escolhido;
-        adicionarJogador1(); 
+        const torneioSelecionado = adicionarJogador1(); //torneio que tem o ID escolhido;
       } else {
         console.log("Insira um ID de um torneio valido!");
         registrarPartidas();
