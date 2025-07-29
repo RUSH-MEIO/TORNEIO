b346915fcbfb9da3ce2cbd62d14b0dff318d6949
OTAVIO <gotaviosonnenstrahl@gmail.com>
2025-07-29 09:31:51 -0300

    ADD

--- torneio.js
+++ torneio.js
@@ -55,7 +55,7 @@ function exibirMenu() {
           adicionarTorneios();
           break;
         case 2:
-          console.log(torneios)
+          listarTorneios();
           break;
         case 3:
           filtrarTorneios();
@@ -154,6 +154,68 @@ async function deletarTorneios(){
     const INPIDDelete = await pergunta("Digite o ID do TORNEIO que deseja deletar")
   }
   
+}
+
+function listarTorneios() {
+  if (torneios.length === 0) {
+    console.clear();
+    console.log('Não há torneios registrados!!');
+  } else {
+    console.clear();
+    console.log('========TORNEIOS========');
+    torneios.forEach((torneio) => {
+      console.log(
+        `ID: ${torneio.id} | Nome: ${torneio.nome} | Jogo: ${torneio.jogo}  | Data: ${torneio.data}`
+      );
+      if (torneio.participantes && Array.isArray(torneio.participantes) && torneio.participantes.length > 0) {
+        console.log('  --- Participante(s) deste Torneio ---');
+        torneio.participantes.forEach((participante) => {
+          console.log(`  - ${participante}`);
+        });
+      } else {
+        console.log('-- Nenhum participante registrado nesse torneio --');
+      }
+      console.log('------------------------------------\n');
+    });
+  }
+  exibirMenu();
+}
+
+function ListarPartidasDoTorneio(){
+    console.clear()
+    if (torneios.partidas && Array.isArray(torneios.partidas) && torneios.partidas.length > 0) {
+        console.log('  --- Partidas deste Torneio ---');
+        torneios.participantes.forEach((partida) => {
+          console.log(`  - ${partida}`);
+        });
+      } else {
+        console.log('----------------------------\nNenhuma partida registrada!!');
+      }
+      console.log('----------------------------\n');
+      exibirMenu()
+    };
+
+function filtrarTorneios() {
+  console.clear();
+  rl.question("Por qual jogo você deseja filtrar?\n", (resposta) => {
+    const jogosFiltrados = torneios.filter(
+      (torneio) => torneio.jogo == resposta
+    );
+    if (jogosFiltrados.length > 0) {
+      console.clear()
+      resposta = resposta.toUpperCase()
+      console.log(`===TORNEIOS COM O JOGO ${resposta}===`)
+      jogosFiltrados.forEach((torneio, index) => {
+        console.log(
+          `ID: ${torneio.id} || Nome: ${torneio.nome} || Jogo: ${torneio.jogo} || Data: ${torneio.data} || Participantes: ${torneio.participantes}`
+        );
+      });
+    } else {
+      console.clear();
+      console.log("Nenhum torneio com este jogo encontrado.");
+    }
+    exibirMenu()
+  });
 }
 
 console.log("Iniciando o sistema...");
